fccd43aa88ceae270d93762f12d3829ee0f282ea
qz515880 <1195933889@qq.com>
2015-07-16 15:44:45 +0800

    修改到可以运行的模板

--- SushiCrush/Classes/AppDelegate.cpp
+++ SushiCrush/Classes/AppDelegate.cpp
@@ -16,7 +16,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     auto director = Director::getInstance();
     auto glview = director->getOpenGLView();
     if(!glview) {
-        glview = GLView::create("Sushi crush");
+		glview = GLViewImpl::create("SushiCrush");
         director->setOpenGLView(glview);
     }
     
